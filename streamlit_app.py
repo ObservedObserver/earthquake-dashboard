5447abcd1d1836459f62a49104c860f25db0a8c7
observedobserver <270001151@qq.com>
2023-09-27 20:24:14 -0700

    feat: fix time format

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,6 @@ import streamlit.components.v1 as components
 import streamlit as st
 from pygwalker.api.streamlit import init_streamlit_comm, get_streamlit_html
 
-st.set_page_config(
-    page_title="Earthquake Visualization with pygwalker",
-    layout="wide"
-)
-
-st.title("Earthquake Visualization (1900-2023) with pygwalker")
-
-# Initialize pygwalker communication
-init_streamlit_comm()
-
 # When using `use_kernel_calc=True`, you should cache your pygwalker html, if you don't want your memory to explode
 @st.cache_resource
 def get_pyg_html(df: pd.DataFrame) -> str:
@@ -22,7 +12,19 @@ def get_pyg_html(df: pd.DataFrame) -> str:
 
 @st.cache_data
 def get_df() -> pd.DataFrame:
-    return pd.read_csv("./Significant Earthquake_Dataset_1900_2023.csv")
+    df = pd.read_csv("./Significant Earthquake_Dataset_1900_2023.csv")
+    df["Time"] = pd.to_datetime(df["Time"]).dt.strftime('%Y-%m-%d %H:%M:%S')
+
+    return df
+
+st.set_page_config(
+    page_title="Earthquake Visualization with pygwalker",
+    layout="wide"
+)
+init_streamlit_comm()
+
+st.title("Earthquake Visualization (1900-2023) with pygwalker")
+st.markdown("""Use [pygwalker](https://github.com/kanaries/pygwalker) for interactive visualization of geospatial data. More docs [here](https://docs.kanaries.net/pygwalker).)""")
 
 df = get_df()
 
